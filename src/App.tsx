ffe038c8c510758c1ab9d9dd2a8660ad0298fe01
Rafael Moser <rafaelpmoser@gmail.com>
2023-05-06 00:50:55 -0300

    aboutme page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./pages/Root";
 import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/aboutme", element: <AboutMe /> },
+    ],
   },
 ]);
 
--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,5 +1,38 @@
+import SkillList, { Skill } from "../modules/SkillList/SkillList";
+const TEST_SKILLS: Skill[] = [
+  {
+    id: "1",
+    level: 4,
+    title: "React",
+    comment: "This blog frontend is made in react",
+    children: [
+      {
+        id: "1.1",
+        level: 3,
+        title: "React Router",
+        comment: "This blog frontend uses react router",
+        children: [],
+      },
+      {
+        id: "1.2",
+        level: 2,
+        title: "nextJS",
+        comment: "done a small project with it",
+        children: [],
+      },
+    ],
+  },
+  {
+    id: "2",
+    level: 3,
+    title: "Python",
+    comment: "This blog backend is made in python",
+    children: [],
+  },
+];
+
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  return <SkillList skills={TEST_SKILLS} />;
 };
 
 export default AboutMe;
