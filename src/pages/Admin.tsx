8aa69dbb75c9311ef79a5961469a268e0bdeca53
Rafael Moser <rafaelpmoser@gmail.com>
2023-05-19 22:08:14 -0300

    Error stuff

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,12 +1,12 @@
 import ModuleSelector from "../modules/admin/ModuleSelector";
 import { useAccessToken } from "../util/Hooks";
+import { ErrorResponse } from "@remix-run/router";
 
 const AdminPage = () => {
   const { hasToken } = useAccessToken();
   if (!hasToken) {
-    throw new Response("Unauthorized", { status: 401 });
+    throw new ErrorResponse(401, "Unauthorized", { data: "Unauthorized" });
   }
-
   return (
     <div className="flex flex-col space-y-8 items-center p-10">
       <text className="text-5xl font-bold">Admin Page</text>
--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -11,6 +11,8 @@ const ErrorPage = () => {
   let errorMessage: string;
   let errorStatus = 0;
 
+  console.log(error);
+
   if (isRouteErrorResponse(error)) {
     // error is type `ErrorResponse`
     errorMessage = error.statusText;
@@ -25,28 +27,20 @@ const ErrorPage = () => {
   }
 
   return (
-    <div className="base-bg text-center h-screen flex justify-center items-center">
+    <div className="base-bg h-screen flex justify-center items-center">
       <div className="element-bg flex flex-col justify-center items-center w-96 h-80 rounded-3xl p-10 shadow-md gap-y-4">
         {errorStatus && <h1 className="text-bold text-9xl">{errorStatus}</h1>}
         <p>{errorMessage}</p>
         <div className="flex flex-row justify-stretch rounded-xl shadow-md divide-x divide-slate-800 w-80">
           <a
             onClick={() => navigate(-1)}
-            className={`font-bold px-4 grow w-40 h-8
-          bg-gradient-to-b from-slate-700 via-slate-500 to-slate-700 
-          hover:from-slate-700 hover:via-slate-400 hover:to-slate-700 
-          active:from-slate-700 active:via-slate-800 active:to-slate-700 
-          rounded-l-xl`}
+            className={`flex items-center justify-center clickable-bg font-bold px-4 grow w-40 h-8 rounded-l-xl`}
           >
             Back
           </a>
           <Link
             to={"/"}
-            className={`font-bold px-4 grow w-40 h-8
-          bg-gradient-to-b from-slate-700 via-slate-500 to-slate-700 
-          hover:from-slate-700 hover:via-slate-400 hover:to-slate-700 
-          active:from-slate-700 active:via-slate-800 active:to-slate-700 
-          rounded-r-xl`}
+            className={`flex items-center justify-center clickable-bg font-bold px-4 grow w-40 h-8 rounded-r-xl`}
           >
             Front page
           </Link>
